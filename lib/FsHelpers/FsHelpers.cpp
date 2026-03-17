6e7b734783a5fd53cbecabb7ec87302ebbfee583
Jake Lipson <jake_lipson@yahoo.com>
2026-03-16 23:47:02 -0400

    Add getCachePath function to generate cache paths based on book type

--- lib/FsHelpers/FsHelpers.cpp
+++ lib/FsHelpers/FsHelpers.cpp
@@ -78,4 +78,10 @@ bool hasTxtExtension(std::string_view fileName) { return checkFileExtension(file
 
 bool hasMarkdownExtension(std::string_view fileName) { return checkFileExtension(fileName, ".md"); }
 
+std::string getCachePath(const std::string& bookPath) {
+  std::string cacheDir = "/.crosspoint";
+  std::string typePrefix = hasXtcExtension(bookPath) ? "/xtc_" : "/epub_";
+  return cacheDir + typePrefix + std::to_string(std::hash<std::string>{}(bookPath));
+}
+
 }  // namespace FsHelpers
